bd9a5bf14210818b797adc304e555c2a490bb9a9
HARISHKALVA <harishkalva14@gmail.com>
2022-07-03 20:56:09 +0530

    geolocation

--- backend/api/open_api.py
+++ backend/api/open_api.py
@@ -1,14 +1,18 @@
-import requests as ress
-
 import requests
+import pandas as pd
+import geocoder
 
-url = "https://forecast9.p.rapidapi.com/"
+latitude = geocoder.ip('me').latlng[0]
+longitude = geocoder.ip('me').latlng[1]
+print(latitude,longitude)
+url = f"https://forecast9.p.rapidapi.com/rapidapi/forecast/{latitude}/{longitude}/hourly/"
 
 headers = {
-	"X-RapidAPI-Key": "edbc9ff912msha49ff4739ed0816p184181jsn80dae3da1b5c",
-	"X-RapidAPI-Host": "forecast9.p.rapidapi.com"
+    "X-RapidAPI-Key": "edbc9ff912msha49ff4739ed0816p184181jsn80dae3da1b5c",
+    "X-RapidAPI-Host": "forecast9.p.rapidapi.com"
 }
 
 response = requests.request("GET", url, headers=headers)
 
 print(response.text)
+print(pd.DataFrame(response.json()))
